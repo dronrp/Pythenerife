0bc5d450fe403422c948bea5a56f02440b6b9c3d
dronrp <64811222+dronrp@users.noreply.github.com>
2021-04-04 13:47:17 +0100

    Templates

---
+++ Templates/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    return 0;
+}
